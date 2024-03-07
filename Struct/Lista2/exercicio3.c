4fe687a0d306f0aa8a8b8c3e4fd43eb69aeac6c7
DiogoKitahara <diogokitahara@gmail.com>
2024-03-06 21:18:44 -0300

    added

--- Struct/Lista2/exercicio3.c
+++ Struct/Lista2/exercicio3.c
@@ -4,8 +4,30 @@ struct paciente {
 	float peso, altura;
 };
 
-calcularImc(struct paciente* p) {
-	
+float calcularImc(struct paciente* p) {
+	float resultado = p.peso / (p.altura * p.altura);
+	return resultado
+}
+
+void imprimirResultado(float imc) {
+	if (imc < 18.5) {
+		printf("Baixo peso\n");
+	}
+	else if (imc < 24.9) {
+		printf("Peso normal\n");
+	}
+	else if (imc < 29.9) {
+		printf("Sobrepeso\n");
+	}
+	else if (imc < 34.9) {
+		printf("Obesidade grau I\n");
+	}
+	else if (imc < 39.9) {
+		printf("Obesidade grau II\n");
+	}
+	else{
+		printf("Obesidade grau III\n");
+	}
 }
 
 int main() {

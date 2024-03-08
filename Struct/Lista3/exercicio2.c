bb045237142ec6c7d15238a652a583844e04c013
DiogoKitahara <diogokitahara@gmail.com>
2024-03-08 20:34:26 -0300

    added

--- Struct/Lista3/exercicio2.c
+++ Struct/Lista3/exercicio2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define TF 3
 
 typedef struct {
 	int dia, mes, ano;
@@ -9,8 +10,34 @@ typedef struct {
 	int codigo, idade;
 	float salario;
 	data d;
-}fucionario;
+}funcionario;
+
+void ler(funcionario f[]) {
+	int i;
+	for(i = 0; i < TF; i++) {
+		printf("Nome: ");
+		fflush(stdin);
+		gets(f.nome);
+		printf("Sexo: ");
+		fflush(stdin);
+		scanf("%c", f.sexo);
+		printf("CPF: ");
+		fflush(stdin);
+		gets(f.cpf);
+		printf("Cargo: ");
+		fflush(stdin);
+		gets(f.cargo);
+		printf("Codigo: ");
+		scanf("%d", f.codigo);
+		printf("Idade: ");
+		scanf("%d", f.idade);
+		printf("Salario: ");
+		scanf("%f", f.salario);
+	}
+}
 
 int main() {
-	
+	funcionario f[TF];
+	ler(f);
+	imprimir(f);
 }
